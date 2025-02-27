e896c83711ce66e0a7f0fd2f27c75965e2e85f7b
Vitaliy <moya.pochta.loginov@gmail.com>
2025-02-27 16:35:06 +0300

    open store time

--- guess_number.py
+++ guess_number.py
@@ -1,17 +1,58 @@
-def find_max_odd_number(iterable_numbers):
-    """
-    Ищет максимальное чётное значение в списке положительных целых значений.
-    Переданном в параметр функции.
-    """
-    current_max = 0
-    for iterable_number in iterable_numbers:
-        if iterable_number % 2 == 0:
-            current_max = max(iterable_number, current_max)
-    return current_max
+# Импортируйте нужную библиотеку.
+from datetime import datetime, date
+
+class Store:
+    def __init__(self, address):
+        self.address: str = address
+
+    def __is_open(self, date) -> bool:
+        # Метод __is_open() в родительском классе всегда возвращает False,
+        # это "код-заглушка", метод, предназначенный для переопределения
+        # в дочерних классах.
+        # Не переопределяйте содержимое этого метода.
+        return False
+
+    def get_info(self, text_date) -> str:
+        # С помощью шаблона даты преобразуйте строку date_str в объект даты:
+        date_object = datetime.strptime(text_date, '%Y-%m-%d')
+
+        # Передайте в метод __is_open() объект даты date_object и определите,
+        # работает ли магазин в указанную дату.
+        # В зависимости от результата будет выбрано значение
+        # для переменной info.
+        if self.__is_open(date_object):
+            info = 'работает'
+        else:
+            info = 'не работает'
+        return f'Магазин по адресу {self.address} {text_date} {info}'
 
 
-max_odd = find_max_odd_number([1, 2, 3, 4, 5])
-# Попробуйте передать в find_max_odd_number() другие списки:
-"""[10, 8, 6, 4, 2]"""
-"""[2, 12, 85, 0, 6]"""
-print(f'Максимальное чётное число: {max_odd}')
+class MiniStore(Store):
+    # Переопределите метод __is_open().
+    # Обратите внимание на имя метода/name mangling
+    def _Store__is_open(self, date: datetime) -> bool:
+
+
+class WeekendStore(Store):
+    # Переопределите метод __is_open().
+    # Обратите внимание на имя метода/name mangling
+    def _Store__is_open(self, date: datetime) -> bool:
+
+
+class NonStopStore(Store):
+    # Переопределите метод __is_open().
+    # Обратите внимание на имя метода/name mangling
+    def _Store__is_open(self, date: datetime) -> bool:
+
+
+mini_store = MiniStore('Улица Немига, 57')
+print(mini_store.get_info('29.03.2024'))
+print(mini_store.get_info('30.03.2024'))
+
+weekend_store = WeekendStore('Улица Толе би, 321')
+print(weekend_store.get_info('29.03.2024'))
+print(weekend_store.get_info('30.03.2024'))
+
+non_stop_store = NonStopStore('Улица Арбат, 60')
+print(non_stop_store.get_info('29.03.2024'))
+print(non_stop_store.get_info('30.03.2024'))
